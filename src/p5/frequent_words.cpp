acb6fe374f762f40fa2541ba47ed2a5ea390503d
Max Morgunov <maxexmachina@gmail.com>
2022-09-11 17:04:00 +0300

    Done without reverse

--- src/p5/frequent_words.cpp
+++ src/p5/frequent_words.cpp
@@ -31,26 +31,19 @@ auto neighbors(std::string_view pattern, std::size_t d) -> std::vector<std::stri
     return neighborhood;
 }
 
-auto find_freq_words_with_mismatches(std::string_view text, std::size_t k, std::size_t d) -> std::vector<std::string_view> {
-    std::vector<std::string_view> patterns{};
-    std::map<std::string_view, std::size_t> freq_map{};
-    for (std::size_t i = 0; i < text.size() - k; ++i) {
-        const auto neighborhood = neighbors(text.substr(i, k), d);
-        fmt::print("Neighborhood: \n");
-        for (const auto & n : neighborhood) {
-            fmt::print("{}\n", n);
-        }
-        fmt::print("Neighborhood ended\n");
+auto find_freq_words_with_mismatches(std::string_view text, std::size_t k, std::size_t d) -> std::vector<std::string> {
+    std::string t{text};
+
+    std::vector<std::string> patterns{};
+    std::map<std::string, std::size_t> freq_map{};
+    for (std::size_t i = 0; i < t.size() - k; ++i) {
+        const auto neighborhood = neighbors(t.substr(i, k), d);
         for (const auto & neighbor : neighborhood) {
             ++freq_map[neighbor];
         }
     }
-    for (const auto & pair : freq_map) {
-        fmt::print("{} : {}\n", pair.first, pair.second);
-    }
     const auto [m_pattern, m] = *std::max_element(freq_map.cbegin(), freq_map.cend(),
                                                   [](auto p1, auto p2){ return p1.second < p2.second; }); 
-    fmt::print("Max pattern : {} : {}", m_pattern, m);
     for (const auto & pair : freq_map) {
         if (freq_map[pair.first] == m) {
             patterns.push_back(pair.first);
@@ -60,14 +53,12 @@ auto find_freq_words_with_mismatches(std::string_view text, std::size_t k, std::
 }
 
 void solve() {
-    const auto str = read_file_to_string("samples/FrequentWords/sample.txt");
+    const auto str = read_file_to_string("samples/FrequentWords/dataset_240221_9.txt");
     const auto [genome, numString] = split_once(str, "\n");
 
     std::size_t k = 0, d = 0;
     std::stringstream numStream{numString.data()};
     numStream >> k >> d;
-
-    fmt::print("k : {}, d : {}\n", k, d);
 
     for (const auto & pattern : find_freq_words_with_mismatches(genome, k, d)) {
         fmt::print("{} ", pattern);
--- src/p5/frequent_words.hpp
+++ src/p5/frequent_words.hpp
@@ -5,6 +5,6 @@
 
 namespace p5 {
 auto neighbors(std::string_view text, std::size_t d) -> std::vector<std::string>;
-auto find_freq_words_with_mismatches(std::string_view text, std::size_t k, std::size_t d) -> std::vector<std::string_view>;
+auto find_freq_words_with_mismatches(std::string_view text, std::size_t k, std::size_t d) -> std::vector<std::string>;
 void solve();
 }
